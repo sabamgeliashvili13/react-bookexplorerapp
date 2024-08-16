19811aca38e921f824dc7072c46e98ddf1fc5633
saba saba <sba69717@gmail.com>
2024-08-16 17:12:37 +0400

    add dark mode

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React from 'react';
 import { useState, useEffect } from 'react';
 import SearchBar from './components/search'; 
 import BookList from './components/bookList'; 
@@ -8,17 +9,24 @@ import { searchBooks } from './components/Api';
 const Button = styled.button`
   padding: 10px 20px;
   border: none;
-  border-radius: 0 5px 5px 0;
   background-color: #810100;
   color: white;
   font-size: 16px;
   cursor: pointer;
+  
+  
+  &:hover {
+    background-color: #710110 ;
+    transition: .3s ease-in-out;
+    border-radius: 10px;
+  }
 `;
 
 function App() {
   const [books, setBooks] = useState([]); 
   const [favourites, setFavourites] = useState([]); 
   const [viewFavourites, setViewFavourites] = useState(false); 
+  const [theme, setTheme] = useState('light');
 
   function handleSearch(query) {
     searchBooks(query).then(function(results) {
@@ -45,12 +53,27 @@ function App() {
     });
   }, []);
 
+  function toggleTheme()  {
+    if (theme === 'light') {
+      setTheme('dark');
+    } else {
+      setTheme('light');
+    }
+  };
+
+  useEffect(() => {
+    document.body.className = theme + '-mode';
+  }, [theme]);
+
   return (
     <div className="App">
       <header className="App-header">
         <Button onClick={toggleViewFavourites}>
           {viewFavourites ? 'Back to Search Results' : 'View Favourite Books'}
         </Button>
+      <Button onClick={toggleTheme}>
+        Switch to {theme === 'light' ? 'Dark' : 'Light'} Mode
+      </Button>
       </header>
       {!viewFavourites && <SearchBar onSearch={handleSearch} />}
       <BookList 
